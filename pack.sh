f69fd1ee8744affa174196977bb17200b66c6dc3
Einar Ingebrigtsen <einar@dolittle.com>
2016-12-07 12:20:59 +0100

    Putting in manual nuspec and pack script

--- pack.sh
+++ pack.sh
@@ -1,2 +1,15 @@
 #! /bin/bash
-dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
+rm -rf artefacts
+mkdir artefacts
+mkdir artefacts/lib
+mkdir artefacts/lib/netcoreapp1.0
+cd ./Source
+dotnet build
+cd ..
+cp ./Source/bin/Debug/netcoreapp1.0/*.dll ./artefacts/lib/netcoreapp1.0
+cp ./Source/bin/Debug/netcoreapp1.0/*.pdb ./artefacts/lib/netcoreapp1.0
+cp ./Source/Package.nuspec ./artefacts
+cd artefacts
+nuget pack Package.nuspec -symbols -version 1.0.0 -suffix nightly001 
+
+#dotnet pack ./Source -c Release -o artefacts --version-suffix nightly001
